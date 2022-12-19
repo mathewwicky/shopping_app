f40ee2e2488baa6ba48d107e35ed5ff290dca6cc
mathewwicky <mathewwicky8@gmail.com>
2022-12-19 15:16:44 +0300

    styling the profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    def __str__(self):
+        return self.user.username
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='profile.jpg',upload_to='profile_pictures')
+    contact_number = models.CharField(max_length=100,default='99999999')
