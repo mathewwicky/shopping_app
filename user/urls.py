c812e8989523d400b50c01fb4da85dfca5a72a3d
mathewwicky <mathewwicky8@gmail.com>
2022-12-19 12:59:42 +0300

    Added the profile page

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register,name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('profile/',views.profile,name='profile'),
    ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import UserForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -16,3 +17,8 @@ def register(request):
     }
 
     return render(request,'user/register.html',context)
+    
+@login_required
+def profile(request):
+    return render(request,'user/profile.html')
+
